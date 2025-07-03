a725e57c8bb060e306db8c08548c99cf8d4821cc
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-07-03 11:19:37 +0200

    Add scripts to handle hypnotized person's animations

---
+++ Assets/Scripts/Characters/CharacterType.cs
@@ -0,0 +1,21 @@
+namespace HypNot.Behaviours.Characters
+{
+   public enum CharacterType
+   {
+      BEARDED_MAN,
+      BIKER,
+      BOY,
+      DETECTIVE,
+      GIRL,
+      LITTLE_BOY,
+      LITTLE_GIRL,
+      MAN,
+      MAN_WITH_GLASSES,
+      OLD_MAN,
+      OLD_WOMAN,
+      VERY_OLD_MAN,
+      WOMAN,
+      YOUNG_MAN,
+      YOUNG_WOMAN
+   }
+}
