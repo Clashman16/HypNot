96666667d7b5079b9b1f7e90eb28239a7d6ecbfe
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-07-08 20:50:49 +0200

    Add new script for citizen

---
+++ Assets/Scripts/Characters/CitizenBehaviour.cs
@@ -0,0 +1,55 @@
+using HypNot.Map;
+using HypNot.Player;
+using HypNot.Spawners;
+using Pathfinding;
+using UnityEngine;
+
+namespace HypNot.Behaviours.Characters
+{
+   public class CitizenBehaviour : MonoBehaviour
+   {
+      private CharacterType m_type;
+
+      public CharacterType Type
+      {
+         get => m_type;
+         set => m_type = value;
+      }
+
+      private HypnotizedPersonTargetBehaviour m_target;
+
+      public HypnotizedPersonTargetBehaviour Target
+      {
+         set => m_target = value;
+      }
+
+      private AIPath m_aiPath;
+
+      public AIPath AIPath
+      {
+         set => m_aiPath = value;
+      }
+
+      private void OnTriggerEnter2D(Collider2D p_collider)
+      {
+         HypnotizedPersonTargetBehaviour l_collidedPerson = p_collider.GetComponent<HypnotizedPersonTargetBehaviour>();
+
+         if (l_collidedPerson != null && m_target == l_collidedPerson)
+         {
+            m_aiPath.canMove = false;
+            m_aiPath.canSearch = false;
+
+            m_target.Citizens.Add(this);
+
+            m_target.Data.ManaCount -= 1;
+
+            if (MapManagerSingleton.Instance.LastSpawnedCitizen == gameObject)
+            {
+               PlayerStateSingleton.Instance.CanSendCitizen = true;
+            }
+
+            HypnotizedPersonSpawnerSingleton.Instance.Spawn();
+         }
+      }
+   }
+}
