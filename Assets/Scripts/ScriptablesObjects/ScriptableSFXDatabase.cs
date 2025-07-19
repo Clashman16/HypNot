2136b1ea2d65a350bedede528a4f0505b37dd224
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-07-19 17:07:21 +0200

    Add last SFX in the database

--- Assets/Scripts/ScriptablesObjects/ScriptableSFXDatabase.cs
+++ Assets/Scripts/ScriptablesObjects/ScriptableSFXDatabase.cs
@@ -7,10 +7,36 @@ namespace HypNot.ScriptableObjects
    public class ScriptableSFXDatabase : ScriptableObject
    {
       [SerializeField]
-      private AudioClip m_victorySound;
+      private AudioClip m_endSound;
+
+      public AudioClip EndSound
+      {
+         get => m_endSound;
+      }
 
       [SerializeField]
-      private AudioClip m_defeatSound;
+      private AudioClip m_collisionSound;
+
+      public AudioClip CollisionSound
+      {
+         get => m_collisionSound;
+      }
+
+      [SerializeField]
+      private AudioClip m_oneMoreCitizenSound;
+
+      public AudioClip OneMoreCitizenSound
+      {
+         get => m_oneMoreCitizenSound;
+      }
+
+      [SerializeField]
+      private AudioClip m_buttonSound;
+
+      public AudioClip ButtonSound
+      {
+         get => m_buttonSound;
+      }
 
       [SerializeField]
       private AudioClip[] m_footstepsSounds;
