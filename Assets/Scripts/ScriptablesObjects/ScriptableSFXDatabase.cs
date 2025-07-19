c305c16e9c069d3a74e48ddfc26c3a4291cd84cf
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-07-19 16:03:51 +0200

    Make citizen to make sounds when he is walking

---
+++ Assets/Scripts/ScriptablesObjects/ScriptableSFXDatabase.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HypNot.ScriptableObjects
+{
+   [CreateAssetMenu(menuName = "ScriptableObjects/SFXDatabase")]
+   public class ScriptableSFXDatabase : ScriptableObject
+   {
+      [SerializeField]
+      private AudioClip m_buttonClickSound;
+
+      [SerializeField]
+      private AudioClip m_victorySound;
+
+      [SerializeField]
+      private AudioClip m_defeatSound;
+
+      [SerializeField]
+      private AudioClip[] m_footstepsSounds;
+
+      public AudioClip[] FootstepsSounds
+      {
+         get => m_footstepsSounds;
+      }
+   }
+}
