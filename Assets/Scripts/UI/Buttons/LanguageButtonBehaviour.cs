0cabf66eeddcbdb16f1e5676458278e48c2656d4
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-08-03 14:11:53 +0200

    Make language buttons to be disbaled

--- Assets/Scripts/UI/Buttons/LanguageButtonBehaviour.cs
+++ Assets/Scripts/UI/Buttons/LanguageButtonBehaviour.cs
@@ -1,9 +1,40 @@
 using HypNot.Player;
+using UnityEngine.UI;
 
 namespace HypNot.Behaviours.UI
 {
    public class LanguageButtonBehaviour : OneActionButtonBehaviour
    {
+      private Button m_languageButton;
+
+      private Button m_otherLanguageButton;
+
+
+      public void Reset()
+      {
+         if(m_languageButton == null)
+         {
+            LanguageButtonBehaviour[] l_allLanguageButtons = transform.parent.GetComponentsInChildren<LanguageButtonBehaviour>();
+
+            foreach (LanguageButtonBehaviour l_button in l_allLanguageButtons)
+            {
+               if (l_button != this)
+               {
+                  m_otherLanguageButton = l_button.GetComponent<Button>();
+               }
+               else
+               {
+                  m_languageButton = l_button.GetComponent<Button>();
+               }
+            }
+         }
+         
+         if(name.ToLower().Contains(PlayerSaveSingleton.Instance.LanguageId))
+         {
+            m_languageButton.interactable = false;
+         }
+      }
+
       public override void Act()
       {
          base.Act();
@@ -11,6 +42,10 @@ namespace HypNot.Behaviours.UI
          string l_languageId = PlayerSaveSingleton.Instance.LanguageId;
 
          PlayerSaveSingleton.Instance.LanguageId = l_languageId == "english" ? "french" : "english";
+
+         m_otherLanguageButton.interactable = true;
+
+         m_languageButton.interactable = false;
       }
    }
 }
